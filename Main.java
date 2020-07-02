fdeeef2a1584ae44b091d3e29d9989d223d2265c
Antonio Santos <antoniosantosramos@gmail.com>
2020-07-03 00:49:01 +0200

    octavo commit

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hola a todos y todas en development");
+		System.out.println("hola a todos y todas en development")
 	}
 
 }
